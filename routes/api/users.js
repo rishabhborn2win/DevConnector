814e16426901d2f388b99127d8454a8e08a7d9a5
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2020-10-27 00:18:34 +0530

    signup-login completed with token
    JWT token genration auth completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult  } = require("express-validator");
     
 const User = require('../../models/Users')
@@ -61,8 +63,21 @@ router.post('/', [
      await user.save()
 
      //Return JWT
-    
-     res.send('Registered')
+
+     const payload ={
+         user: {
+            id: user.id
+         }
+     }
+
+     jwt.sign(
+         payload,
+         config.get('JWTsecret'),
+         { expiresIn: 360000},
+         (err, token) => {
+            if(err) console.log(err);
+            res.json({ token });
+         });
      }catch(err){
         if(err){
             console.log(err.message);
