ffd284105e05bc9fff191089951b3b50076faec8
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2020-10-28 20:46:34 +0530

    manage post

---
+++ models/Post.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const User = require('./Users');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    text:{
+        type: String,
+        required: true
+    },
+    name:{
+        type: String
+    },
+    avatar:{
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                tyep: String
+            },
+            avatar: {
+                type: String
+            },
+            date:{
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+
+    
+});
+
+module.exports = Post = mongoose.model('post', postSchema);
