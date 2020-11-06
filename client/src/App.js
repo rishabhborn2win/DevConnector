57a044e5ea3cc6d28c01f35c1a42c80b67c64f6c
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2020-11-06 19:18:09 +0530

    updating frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' ;
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+//Redux
+import { Provider }  from 'react-redux';
+import store from './store'
+import Alert from './components/layout/Alert'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+<Provider store={store}>
+<Router>
+  <Fragment>
+    <Navbar />
+    <Route exact path='/' component={Landing} />
+    <section className="container">
+      <Alert />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+
+      </Switch>
+    </section>
+  </Fragment>
+</Router>
+</Provider>
+);
+
 
 export default App;
