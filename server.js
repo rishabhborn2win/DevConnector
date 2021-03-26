396313d705cd2c4ed1c95edf673502e88a0e56ef
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-03-26 19:51:28 +0530

    updates in port

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 786;
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
